3f2a1b095df4c4c62b169e10912f63de6107181f
zhangyang <18610942105@163.com>
2019-10-23 23:00:56 +0800

    revert 3

---
+++ app/src/main/java/com/zy/hellogit/e.java
@@ -0,0 +1,11 @@
+package com.zy.hellogit;
+
+import android.util.Log;
+
+/**
+ * Created by PC on 2019/10/23.
+ */
+
+public class e {
+    Log e;
+}
---
+++ app/src/main/java/com/zy/hellogit/f.java
@@ -0,0 +1,10 @@
+package com.zy.hellogit;
+
+/**
+ * Created by PC on 2019/10/23.
+ */
+
+public class f {
+    fjj;
+
+}
